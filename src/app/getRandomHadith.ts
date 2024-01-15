ce8a51121d0d6e86a34bf6750fe6a515e9850849
os-mhmd <docosmo010@gmail.com>
2024-01-15 21:47:57 +0200

    feat/if-failed-hadith-response

--- src/app/getRandomHadith.ts
+++ src/app/getRandomHadith.ts
@@ -1,30 +1,6 @@
 import HadithResponse from "@/types/hadith-response";
-const axios = require("axios");
 
-const defaultResponse = {
-  collection: "riyadussalihin",
-  bookNumber: "introduction",
-  chapterId: "11.00",
-  hadithNumber: "98",
-  hadith: [
-    {
-      lang: "en",
-      chapterNumber: "11",
-      chapterTitle: "The Struggle (in the Cause of Allah)",
-      urn: 1600970,
-      body: `<p>'Aishah (May Allah be pleased with her) said: The Prophet (ﷺ) would stand (in prayer) so long that the skin of his feet would crack. I asked him, "Why do you do this while your past and future sins have been forgiven?" He said, "Should I not be a grateful slave of Allah?"<br/><br/><b>[Al-Bukhari and Muslim]</b>.<br/><br/></p>`,
-      grades: [],
-    },
-    {
-      lang: "ar",
-      chapterNumber: "11",
-      chapterTitle: "- باب المجاهدة",
-      urn: 1700970,
-      body: `<p>- الرابع‏:‏ عن عائشة رضي الله عنها أن النبي صلى الله عليه وسلم كان يقوم من الليل حتى تنفطر قدماه، فقلت له‏:‏ لم تصنع هذا يا رسول الله، وقد غفر الله لك ما تقدم من ذنبك وما تأخر‏؟‏‏!‏ قال‏:‏ ‏"‏ أفلا أحب أن أكون عبداً شكوراً‏؟‏‏"‏ ‏(‏‏(‏متفق عليه‏.‏ هذا لفظ البخاري، ونحوه في الصحيحين من رواية المغيرة بن شعبة‏)‏‏)‏‏.‏</p>`,
-      grades: [],
-    },
-  ],
-};
+const axios = require("axios");
 
 const options = {
   method: "GET",
@@ -38,6 +14,6 @@ export async function getRandomHadith() {
     console.log(response.data);
     return response.data;
   } catch (error) {
-    return defaultResponse;
+    return "حاول مجددا";
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,19 @@ import { getRandomHadith } from "@/app/getRandomHadith";
 
 export default async function Hadith() {
   const response = await getRandomHadith();
+  let hadith =
+    typeof response == "string"
+      ? response
+      : response.hadith[1].body.replace(/<p>?- /, "").replace("</p>", "");
 
-  try {
-    let hadith = response.hadith[1].body;
-    return (
-      <div className="bg-[hsl(217,19%,24%)] w-[80%] mx-auto rounded-2xl p-5">
-        <h2 className="text-[2.4rem] w-fit py-6 mx-auto text-[hsl(150,100%,66%)]">
-          حديث
-        </h2>
-        <p className="text-center max-w-prose text-[1.6rem] p-3">
-          {hadith.replace(/<p>?- /, "").replace("</p>", "")}
-        </p>
-      </div>
-    );
-  } catch {
-    return (
-      <div className="flex justify-center items-center h-screen flex-col gap-3">
-        فشل التحميل. حاول مره اخرى
-        <a href={`/hadith`} className="p-3 rounded-md bg-gray-400">
-          حاول مجددا
-        </a>
-      </div>
-    );
-  }
+  return (
+    <div className="bg-[hsl(217,19%,24%)] w-[80%] mx-auto rounded-2xl p-5">
+      <h2 className="text-[2.4rem] w-fit py-6 mx-auto text-[hsl(150,100%,66%)]">
+        حديث
+      </h2>
+      <p className="text-center max-w-prose text-[1.6rem] p-3 mx-auto">
+        {hadith}
+      </p>
+    </div>
+  );
 }
